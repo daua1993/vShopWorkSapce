3f4b09fea8ce9b0a22e35dca15211f387e0c7d8c
daua1993 <daua1993@gmail.com>
2015-05-26 10:35:14 +0700

    Thu xem sao

---
+++ app/src/main/java/com/dtsgroup/vshop/activity/MainActivity.java
@@ -0,0 +1,22 @@
+package com.dtsgroup.vshop.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+/**
+ * Created by DauTV on 26/05/2015.
+ */
+public class MainActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.e("TAG","String Test");
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
